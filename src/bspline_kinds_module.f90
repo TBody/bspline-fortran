e93099565a17795ace711c81c900fba0a7504b47
Thomas Body <thomas.body@ipp.mpg.de>
2020-07-27 13:33:43 +0200

    Apply PARALLAX patch

--- src/bspline_kinds_module.f90
+++ src/bspline_kinds_module.f90
@@ -13,7 +13,12 @@
 
     private
 
-    integer,parameter,public :: wp = real64  !! Real working precision
+#ifdef DOUBLE_PREC
+    integer,parameter,public :: wp = real64  !! Real precision
+#else
+    integer,parameter,public :: wp = real32  !! Real precision
+#endif
+ 
     integer,parameter,public :: ip = int32   !! Integer working precision
 
 !*****************************************************************************************
